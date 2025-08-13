254fea5bf6e1fb3bd41980d4acd0d4c88f20c75c
Alexey Skobkin <skobkin@proton.me>
2025-08-14 01:28:27 +0300

    Format tray files

--- tray/tray_test.go
+++ tray/tray_test.go
@@ -3,38 +3,37 @@ package tray
 import "testing"
 
 func TestNoopCallbacks(t *testing.T) {
-        n := &Noop{}
-        calledShow := false
-        n.OnShowHide(func() { calledShow = true })
-        if n.showHide == nil {
-                t.Fatalf("showHide callback not set")
-        }
-        n.showHide()
-        if !calledShow {
-                t.Fatalf("showHide callback not invoked")
-        }
+	n := &Noop{}
+	calledShow := false
+	n.OnShowHide(func() { calledShow = true })
+	if n.showHide == nil {
+		t.Fatalf("showHide callback not set")
+	}
+	n.showHide()
+	if !calledShow {
+		t.Fatalf("showHide callback not invoked")
+	}
 
-        var enabled bool
-        n.OnToggleNotifications(func(e bool) { enabled = e })
-        if n.toggle == nil {
-                t.Fatalf("toggle callback not set")
-        }
-        n.toggle(true)
-        if !enabled {
-                t.Fatalf("toggle callback not invoked")
-        }
+	var enabled bool
+	n.OnToggleNotifications(func(e bool) { enabled = e })
+	if n.toggle == nil {
+		t.Fatalf("toggle callback not set")
+	}
+	n.toggle(true)
+	if !enabled {
+		t.Fatalf("toggle callback not invoked")
+	}
 
-        exited := false
-        n.OnExit(func() { exited = true })
-        if n.exit == nil {
-                t.Fatalf("exit callback not set")
-        }
-        n.exit()
-        if !exited {
-                t.Fatalf("exit callback not invoked")
-        }
+	exited := false
+	n.OnExit(func() { exited = true })
+	if n.exit == nil {
+		t.Fatalf("exit callback not set")
+	}
+	n.exit()
+	if !exited {
+		t.Fatalf("exit callback not invoked")
+	}
 
-        // Run should be a no-op
-        n.Run()
+	// Run should be a no-op
+	n.Run()
 }
-
