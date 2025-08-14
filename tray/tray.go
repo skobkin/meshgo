640137c9772e6b776f45442bdf1f90c2cdf916e8
Alexey Skobkin <skobkin@proton.me>
2025-08-14 18:17:51 +0300

    Fix startup when tray unavailable and add logging

--- tray/tray.go
+++ tray/tray.go
@@ -1,5 +1,7 @@
 package tray
 
+import "log/slog"
+
 // Tray controls the application tray icon state and routes user interactions.
 type Tray interface {
 	// SetUnread toggles the unread indicator.
@@ -21,6 +23,7 @@ type Noop struct {
 	showHide func()
 	toggle   func(bool)
 	exit     func()
+	quit     chan struct{}
 }
 
 func (n *Noop) SetUnread(bool) {}
@@ -31,6 +34,27 @@ func (n *Noop) OnToggleNotifications(fn func(bool)) { n.toggle = fn }
 
 func (n *Noop) OnExit(fn func()) { n.exit = fn }
 
-func (n *Noop) Run() {}
+// Run blocks until Quit is called.
+func (n *Noop) Run() {
+	if n.quit == nil {
+		n.quit = make(chan struct{})
+	}
+	slog.Info("tray disabled; running without system tray")
+	<-n.quit
+}
 
-func (n *Noop) Quit() {}
+// Quit unblocks Run and calls the exit callback if set.
+func (n *Noop) Quit() {
+	if n.quit != nil {
+		select {
+		case <-n.quit:
+			// already closed
+		default:
+			close(n.quit)
+		}
+	}
+	if n.exit != nil {
+		n.exit()
+	}
+	slog.Info("tray quit")
+}
